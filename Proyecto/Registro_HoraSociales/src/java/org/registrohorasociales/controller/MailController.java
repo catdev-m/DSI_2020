16a710148995cf6066811ababfddedcd2863cfab
Miguel Córdova <Miguel-081992@hotmail.com>
2020-09-22 08:00:49 -0600

    Ingresar solicitud, Envio de correo al confirmar

---
+++ Proyecto/Registro_HoraSociales/src/java/org/registrohorasociales/controller/MailController.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.registrohorasociales.controller;
+
+import java.io.Serializable;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Miguel
+ */
+@ManagedBean
+@ViewScoped
+public class MailController implements Serializable{
+    @PostConstruct
+    public void initialize() {
+    }
+
+    public void accept(String due, String nombre) {
+        Properties p = new Properties();
+        p.setProperty("mail.smtp.host", "smtp.gmail.com");
+        p.setProperty("mail.smtp.starttls.enable", "true");
+        p.setProperty("mail.smtp.port", "587");
+        p.setProperty("mail.smtp.auth", "true");
+
+        Session s = Session.getDefaultInstance(p);
+        String from = "miguedark92@gmail.com";
+        String pass = "mepc1992";
+        String clave = generatePass();
+        String to = due + "@ues.edu.sv";
+        String asunto = "Solicitud aprobada";
+        String mensaje = "Bienvenido! Estás autorizado para iniciar tu servicio social.\n"
+                + "Puedes iniciar sesión en el sistema con las siguientes credenciales\n"
+                + "Carnet: " + due + " \n"
+                + "Contraseña: "+clave+"\n";
+
+        MimeMessage mail = new MimeMessage(s);
+        try {
+            mail.setFrom(new InternetAddress(from));
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mail.setSubject(asunto);
+            mail.setText(mensaje);
+
+            Transport t = s.getTransport("smtp");
+            t.connect(from, pass);
+            t.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+            t.close();
+
+            addUserAprobbed(due, to, nombre, clave);
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Alumno aprobado para su servicio social", ""));
+
+        } catch (MessagingException ex) {
+            Logger.getLogger(solicitudController.class.getName()).log(Level.SEVERE, null, ex);
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Algo ha fallado", ""));
+        }
+        
+    }
+    
+    
+     public void deny(String due, String nombre) {
+        Properties p = new Properties();
+        p.setProperty("mail.smtp.host", "smtp.gmail.com");
+        p.setProperty("mail.smtp.starttls.enable", "true");
+        p.setProperty("mail.smtp.port", "587");
+        p.setProperty("mail.smtp.auth", "true");
+
+        Session s = Session.getDefaultInstance(p);
+        String from = "miguedark92@gmail.com";
+        String pass = "mepc1992";
+        String to = due + "@ues.edu.sv";
+        String asunto = "Solicitud rechazada";
+        String mensaje = nombre+" tu solicitud para iniciar el servicio social ha sido rechazada.\n"
+                + "Visita nuestras instalaciones para saber más\n";
+
+        MimeMessage mail = new MimeMessage(s);
+        try {
+            mail.setFrom(new InternetAddress(from));
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mail.setSubject(asunto);
+            mail.setText(mensaje);
+
+            Transport t = s.getTransport("smtp");
+            t.connect(from, pass);
+            t.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+            t.close();
+
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "La solicitud se ha rechazado", ""));
+            deleteSolicitud(due);
+
+        } catch (MessagingException ex) {
+            Logger.getLogger(solicitudController.class.getName()).log(Level.SEVERE, null, ex);
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Algo ha fallado", ""));
+        }
+        
+    }
+
+
+    public String generatePass() {
+        String pass;
+        int numero = (int) (Math.random() * 9 + 1);
+        pass = String.valueOf(numero);
+        for (int i = 0; i < 4; i++) {
+            numero = (int) (Math.random() * 9 + 1);
+            pass += String.valueOf(numero);
+        }
+        return pass;
+    }
+
+    public void addUserAprobbed(String carnet, String email, String name, String password) {
+        loginSecurity s = new loginSecurity();
+        s.setFormEmail(email);
+        s.setFormUserName(name);
+        s.setFormUser(carnet);
+        s.setFormPassword(password);
+        s.crearUsuario();
+        deleteSolicitud(carnet);
+    }
+    
+    public void deleteSolicitud(String carnet){
+        solicitudController soli = new solicitudController();
+        soli.eliminarCarreraById(carnet);
+    }
+    
+    public void prueba(){
+        System.out.println("Lammada");
+    }
+}
---
+++ Proyecto/Registro_HoraSociales/src/java/org/registrohorasociales/controller/carreraController.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.registrohorasociales.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import org.registrohorasociales.config.ApplicationContextProvider;
+import org.registrohorasociales.entity.Carrera;
+import org.registrohorasociales.repository.CarreraRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+/**
+ *
+ * @author Miguel
+ */
+@ManagedBean
+@ViewScoped
+@SessionScoped
+public class carreraController implements Serializable {
+
+    private String formName,formCode;
+    private CarreraRepository carreraRepo = ApplicationContextProvider.getApplicationContext().getBean(CarreraRepository.class);
+    private List<Carrera> carreras;
+    private List<SelectItem> listaCarreras;
+    private Carrera carreraSelector;
+    
+    
+@PostConstruct
+    public void init() {
+        carreraRepo=ApplicationContextProvider.getApplicationContext().getBean(CarreraRepository.class);
+        loadCarreras();
+    }
+
+    public String crearCarrera() {
+
+        try {
+            Carrera car = new Carrera();
+            car.setCodigocarrera(formCode);
+            car.setNombrecarrera(formName);
+            carreraRepo.save(car);
+            
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO, "Carrera creada con éxito",""));
+        } catch (Exception e) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "No se pudo almacenar la carrera", ""));
+        }
+
+        return null;
+    }
+
+    public void loadCarreras(){
+        carreras = new ArrayList<>();
+        this.listaCarreras= new ArrayList<>();
+        carreras = carreraRepo.carrerasList();
+        listaCarreras.clear();
+        
+        carreras.stream().map((car) -> new SelectItem(car.getIdcarrera(),    car.getNombrecarrera())).forEachOrdered((c) -> {
+            this.listaCarreras.add(c);
+        });
+    }
+    
+    public String findCarreraById(int code){
+        try{
+        carreraRepo = ApplicationContextProvider.getApplicationContext().getBean(CarreraRepository.class);
+        Carrera ca = new Carrera();
+        ca =  carreraRepo.findOne(code);
+        return ca.getNombrecarrera();
+        }catch(EmptyResultDataAccessException ex){
+            throw new RuntimeException("User not exist");
+        }
+    }
+
+    public String getFormName() {
+        return formName;
+    }
+    
+    public void deleteCarreraById(int id){
+        try{
+            carreraRepo.delete(id);
+        }catch(Exception e){
+            
+        }
+    }
+
+
+    public void setFormName(String formName) {
+        this.formName = formName;
+    }
+
+    public String getFormCode() {
+        return formCode;
+    }
+
+    public void setFormCode(String formCode) {
+        this.formCode = formCode;
+    }
+
+    public List<Carrera> getCarreras() {
+        return carreras;
+    }
+
+    public void setCarreras(List<Carrera> carreras) {
+        this.carreras = carreras;
+    }
+
+    public Carrera getCarreraSelector() {
+        return carreraSelector;
+    }
+
+    public void setCarreraSelector(Carrera carreraSelector) {
+        this.carreraSelector = carreraSelector;
+    }
+
+    public List<SelectItem> getListaCarreras() {
+        return listaCarreras;
+    }
+
+    public void setListaCarreras(List<SelectItem> listaCarreras) {
+        this.listaCarreras = listaCarreras;
+    }
+
+
+}
